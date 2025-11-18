5b536797d120d0dd20bae1a536553ceaa9c49dbf
mtakbaba <takbaba@valven.com>
2025-11-18 13:20:06 +0300

    Update relase.py

--- relase.py
+++ relase.py
@@ -12,112 +12,113 @@ sad("1.0.0")
 
 
 
-def count(a):
-  for i in range(len(a)):
-    asd
-    asd
-    sad
-    sad
-    sad
-    asd
-    as
-    das
-    d
-    ad
-    d
-
-
-def count(a):
-  for i in range(len(a)):
-    asd
-    asd
-    sad
-    sad
-    sad
-    asd
-    as
-    das
-    d
-    ad
-    d
-def count(a):
-  for i in range(len(a)):
-    asd
-    asd
-    sad
-    sad
-    sad
-    asd
-    as
-    das
-    d
-    ad
-    d
-def count(a):
-  for i in range(len(a)):
-    asd
-    asd
-    sad
-    sad
-    sad
-    asd
-    as
-    das
-    d
-    ad
-    d
-def count(a):
-  for i in range(len(a)):
-    asd
-    asd
-    sad
-    sad
-    sad
-    asd
-    as
-    das
-    d
-    ad
-    d
-def count(a):
-  for i in range(len(a)):
-    asd
-    asd
-    sad
-    sad
-    sad
-    asd
-    as
-    das
-    d
-    ad
-    d
-def count(a):
-  for i in range(len(a)):
-    asd
-    asd
-    sad
-    sad
-    sad
-    asd
-    as
-    das
-    d
-    ad
-    d
+def count(a):asdasd
+  as
+da
+sd
 asd
+as
+d
+
+  for i in range(len(a)):
+    asd
+    asd
+    sad
+    sad
+    sad
+    asd
+    as
+    das
+    d
+    ad
+    d
+
+
+def count(a):
+  for i in range(len(a)):
+    asd
+    asd
+    sad
+    sad
+    sad
+    asd
+    as
+    das
+    d
+    ad
+    d
+def count(a):
+  for i in range(len(a)):
+    asd
+    asd
+    sad
+    sad
+    sad
+    asd
+    as
+    das
+    d
+    ad
+    d
+def count(a):
+  for i in range(len(a)):
+    asd
+    asd
+    sad
+    sad
+    sad
+    asd
+    as
+    das
+    d
+    ad
+    d
+def count(a):
+  for i in range(len(a)):
+    asd
+    asd
+    sad
+    sad
+    sad
+    asd
+    as
+    das
+    d
+    ad
+    d
+def count(a):
+  for i in range(len(a)):
+    asd
+    asd
+    sad
+    sad
+    sad
+    asd
+    asqwe
+    dasqwe
+    deqweqwe
+    ad
+qw    dqweqe
+wedef count(a):
+  for i in range(len(a)):
+    asdeq
+    sadqw
+    sad
+    sad
+    asdwe
+    as
+    das
+    d
+    ad
+    deq
+asdqw
 asd
 as
 d
 qweqw
 q
-
-
-
-
-123
-123
-
+w
+d
 123
 123124091*23
 a
