574941a2da388299ab048e33e29af04e32c9e31a
mtakbaba <takbaba@valven.com>
2025-11-18 10:40:26 +0300

    Update test..java

--- test..java
+++ test..java
@@ -2,13 +2,32 @@ public static void main(String[] args**) {
   String name = "Tolgaasdasd";  
 asd
   as
-  dsa
-  das
-  dasdasdasdas
+asdasdasd
+  as
   d
-  asdsaasa
-  sa
-
+  asd
   a
+  sd
+
+  qwe
+  q
+  we
+
+  12
+  3
+  123
+
+  as
+  d
+  21
+  3
+
+  54
+
+  6u
+  67
+  8y
+  ıky
+  
   asasas
 }
